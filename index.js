8bd75502bddea6a571474036d65a479125ff7691
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-19 11:46:21 +0300

    Fetched data from API

--- index.js
+++ index.js
@@ -10,82 +10,72 @@ function eminemChange () {
       // console.log(response)
       return response.json()
     })
-    // DOM stuff
     .then(body => {
-      console.log(body)
-      console.log(body.data[1].title)
 
-      let title = document.querySelector(".ultimate-card-eminem .card-body h6")
-      let img = document.querySelector(".ultimate-card-eminem img")
-      let artistName = document.querySelector(".ultimate-card-eminem .card-body p")
+      console.log(body) 
+      console.log(body.data) //array of objects inside the promise
 
-      title.innerHTML = body.data[1].title
-      img.src = body.data[1].album.cover_medium
-      artistName.innerHTML = body.data[1].artist.name
+      const row = document.querySelector(".special-row")
+
+      for(let i = 0; i < body.data.length; i++) {
+        const obj = body.data[i]
+
+        const col = document.createElement("div")
+        col.className = "col-3"
+
+        col.innerHTML = `
+        <div class="card p-2 cb mx-2 mb-4" style="min-width: 150px; max-width: 200px;"> 
+          <img src="${obj.album.cover_medium}" class="card-img-top" alt="..."> 
+          <div class="card-body"> 
+            <h6 class="card-title">${obj.album.title}</h6> 
+          </div> 
+        </div> 
+        `
+
+        row.appendChild(col)
+      }
+
+
+
+      // let title = document.querySelector(".ultimate-card-eminem .card-body h6")
+      // let img = document.querySelector(".ultimate-card-eminem img")
+      // let artistName = document.querySelector(".ultimate-card-eminem .card-body p")
+
+      // title.innerHTML = body.data[1].title
+      // img.src = body.data[1].album.cover_medium
+      // artistName.innerHTML = body.data[1].artist.name
+
+
+
+      // <h5 class="m-4">Recently played</h5>
+
+
+        {/* <div class="container-fluid d-flex justify-content-start flex-wrap"> */}
+
+        {/* <div class="card p-2 cb mx-2 mb-4" style="min-width: 150px; max-width: 200px;"> */}
+        {/* <img src="./assets/album.jpg" class="card-img-top" alt="..."> */}
+        {/* <div class="card-body"> */}
+        {/* <h6 class="card-title">Django Soundtrack</h6> */}
+        {/* <div class="play-btn-2 "></div> */}
+        {/* <a href="./artist-page.html" style="text-decoration-color: white;"> */}
+        {/* <p class="cardtext">Some Great Arist!</p> */}
+        {/* </a> */}
+        {/* </div> */}
+        {/* </div> */}
     })
     .catch(err => {
       console.error(err)
     });
 }
 
-function metallicaChange () {
-  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica", {
-"method": "GET",
-"headers": {
-  "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-  "x-rapidapi-key": "87cf04f455mshd260e6df25f60bap1b8868jsne2efda3b987d"
-}
-})
-    .then(response => {
-      return response.json()
-    })
-    .then(body => {
-      console.log(body.data[1])
+// function metallicaChange () {
+// }
 
-      let titleMetallica = document.querySelector(".ultimate-card-metallica .card-body h6")
-      let imgMetallica = document.querySelector(".ultimate-card-metallica img")
-      let artistNameMetallica = document.querySelector(".ultimate-card-metallica .card-body p")
-
-      titleMetallica.innerHTML = body.data[2].title
-      imgMetallica .src = body.data[1].album.cover_medium
-      artistNameMetallica .innerHTML = body.data[1].artist.name
-    })
-    .catch(err => {
-      console.error(err);
-  })
-}
-
-function behemothChange () {
-  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=behemoth", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    "x-rapidapi-key": "87cf04f455mshd260e6df25f60bap1b8868jsne2efda3b987d"
-  }
-  })
-  .then(response => {
-    return response.json()
-  })
-  .then(body => {
-    console.log("___________________________")
-      console.log(body.data[1])
-
-
-      let titleBehemot = document.querySelector(".ultimate-card-behemot .card-body h6")
-      let imgBehemot = document.querySelector(".ultimate-card-behemot img")
-      let artistNameBehemot = document.querySelector(".ultimate-card-behemot .card-body p")
-
-      titleBehemot.innerHTML = body.data[2].title
-      imgBehemot .src = body.data[1].album.cover_medium
-      artistNameBehemot .innerHTML = body.data[1].artist.name
-    })
-  .catch(err => {
-  console.error(err);
-  })
-}
+// function behemothChange () {
+// }
 
 window.onload = () => {
   eminemChange()
-  metallicaChange()
-  behemothChange()
+  // metallicaChange()
+  // behemothChange()
 }
